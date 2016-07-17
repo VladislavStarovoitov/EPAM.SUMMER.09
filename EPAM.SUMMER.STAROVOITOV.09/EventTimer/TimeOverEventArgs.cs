a56e8f978d3d5ae3bb5dcef841975c050683a1d0
VladislavStarovoitov <VladislavStarovoitov>
2016-07-17 10:54:29 +0300

    Task 1 without testing

---
+++ EPAM.SUMMER.STAROVOITOV.09/EventTimer/TimeOverEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventTimer
+{
+    public sealed class TimeOverEventArgs: EventArgs
+    {
+        private int _time;
+
+        public TimeOverEventArgs(int time)
+        {
+            _time = time;
+        }
+
+        public int Time => _time;
+    }
+}
---
+++ EPAM.SUMMER.STAROVOITOV.09/EventTimer/Timer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace EventTimer
+{
+    public sealed class Timer
+    {
+        private int _time;
+
+        public event EventHandler<TimeOverEventArgs> TimeOver = delegate { };
+        public int Time
+        {
+            get
+            {
+                return _time;
+            }
+            set
+            {
+                if (value >= 0)
+                    _time = value;
+                else
+                    new ArgumentOutOfRangeException(nameof(Time));
+            }
+        }
+
+        public void Start()
+        {
+            Thread.Sleep(_time * 1000);
+            OnTimeOver(new TimeOverEventArgs(Time));
+        }
+
+        private void OnTimeOver(TimeOverEventArgs e)
+        {
+            TimeOver(this, e);
+        }
+    }
+}
