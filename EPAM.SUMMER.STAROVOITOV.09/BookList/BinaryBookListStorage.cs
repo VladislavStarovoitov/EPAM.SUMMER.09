c5e3bacf020775b9c5afeded14209de4a68d477a
VladislavStarovoitov <VladislavStarovoitov>
2016-07-18 23:58:11 +0300

    Add BookListService

---
+++ EPAM.SUMMER.STAROVOITOV.09/BookList/BinaryBookListStorage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookShop;
+
+namespace BookList
+{
+    class BinaryBookListStorage : IBookListStorage
+    {
+        public List<Book> LoadBooks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveBooks(IEnumerable<Book> books)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EPAM.SUMMER.STAROVOITOV.09/BookList/BookListService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookShop;
+
+namespace BookList
+{
+    public class BookListService
+    {
+        private List<Book> _books = new List<Book>();
+
+        public void AddBook(Book book)
+        {
+            if (ReferenceEquals(book, null))
+                throw new ArgumentNullException();
+            if (_books.Contains(book))
+                throw new ArgumentException();
+            _books.Add(book);
+        }
+
+        public void RemoveBook(Book book)
+        {
+            if (ReferenceEquals(book, null))
+                throw new ArgumentNullException();
+            if (!(_books.Remove(book)))
+                throw new ArgumentException(); 
+        }
+
+        public Book FindBookByTag(Predicate<Book> match)
+        {
+            if (ReferenceEquals(match, null))
+                throw new ArgumentNullException();
+            return _books.Find(match);
+        }
+
+        public void SortBooksByTag(Comparison<Book> comparer)
+        {
+            if (ReferenceEquals(comparer, null))
+                throw new ArgumentNullException();
+            _books.Sort(comparer);
+        }
+    }
+}
