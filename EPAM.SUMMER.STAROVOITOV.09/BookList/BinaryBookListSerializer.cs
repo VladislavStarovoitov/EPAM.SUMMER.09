cf62e5214e0bf1f8070be2692a362e01696c8218
VladislavStarovoitov <VladislavStarovoitov>
2016-07-26 19:23:12 +0300

    Add serializer for BookListService

---
+++ EPAM.SUMMER.STAROVOITOV.09/BookList/BinaryBookListSerializer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookShop;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace BookList
+{
+    class BinaryBookListSerializer : IBookListStorage
+    {
+        public BinaryBookListSerializer(string fileName)
+        {
+            if (fileName != string.Empty)
+                FileName = fileName;
+            throw new ArgumentException();
+        }
+
+        public string FileName { get; }
+
+        public List<Book> LoadBooks()
+        {
+            List<Book> result;
+            BinaryFormatter formatter = new BinaryFormatter();
+            if (!File.Exists(FileName))
+                throw new FileNotFoundException();
+            try
+            {
+                using (var fileStream = File.OpenRead(FileName))
+                {
+                    result = (List<Book>)formatter.Deserialize(fileStream);
+                }
+            }
+            catch (IOException e)
+            {
+                throw e;
+            }
+            return result;
+        }
+
+        public void SaveBooks(IEnumerable<Book> books)
+        {
+            var bookList = books.ToList<Book>();
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            using (var fileStream = File.OpenWrite(FileName))
+            {
+                formatter.Serialize(fileStream, bookList);
+            }
+        }
+    }
+}
